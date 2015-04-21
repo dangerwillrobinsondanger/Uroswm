8975d6bea1fb77b66a9ef2ed50e6fedd18ae0f40
AlessandroSangiuliano <alex22_7@hotmail.com>
2015-04-21 13:42:03 +0200

    Splitting the event handlder in a proper class

---
+++ URNotificationHandler.h
@@ -0,0 +1,26 @@
+/*
+   Project: uroswm
+
+   Copyright (C) 2015 Alex Sangiuliano
+
+   Author: Alex Sangiuliano
+
+   Created: 2015-04-21 13:34:47 +0200 by Alex Sangiuliano
+
+*/
+
+/**
+* This class has the purpose to handle the X notifications, like CreateNotify, 
+* DestroyNotify, MotionNoify and so on.
+*/
+
+#ifndef _URNOTIFICATIONHANDLER_H_
+#define _URNOTIFICATIONHANDLER_H_
+
+#import <Foundation/Foundation.h>
+
+@interface URNotificationHandler : NSObject
+
+@end
+
+#endif _URNOTIFICATIONHANDLER_H_
---
+++ URNotificationHandler.m
@@ -0,0 +1,16 @@
+/*
+   Project: uroswm
+
+   Copyright (C) 2015 Alex Sangiuliano
+
+   Author: Alex Sangiuliano
+
+   Created: 2015-04-21 13:34:47 +0200 by Alex Sangiuliano
+
+*/
+
+#import "URNotificationHandler.h"
+
+@implementation URNotificationHandler
+
+@end
