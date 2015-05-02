d12b9890875a7a86f1904f9511e541b748a736fb
AlessandroSangiuliano <alex22_7@hotmail.com>
2015-05-02 11:56:54 +0200

    Forgot to commit the resize code

--- URNotificationHandler.m
+++ URNotificationHandler.m
@@ -36,11 +36,6 @@
 
 /*- (void) handleCreateNotifyEvent:(XEvent)theEvent
 {
-    NSLog(@"The Create event to handle: %d", theEvent.type);
-    XCreateWindowEvent createEvent = theEvent.xcreatewindow;
-    URWindow *win = [[URWindow alloc] initWithXWindow:createEvent.window display:display];
-    NSString *key = [NSString stringWithFormat:@"%lu",createEvent.window];
-    //[windowsDict setObject:win forKey:key];
 }*/
 
 - (void) handleDestroyNotifyEvent:(XEvent)theEvent
@@ -53,29 +48,14 @@
        NSLog(@"Nil window in destroy");
        return;
     }
-
-    //XUnmapSubwindows(display, [win frameWindow]);
-    //XUnmapWindow(display,[win frameWindow]);
-    //XReparentWindow(display,[win xWindow],[win rootWindow],0,0);
-    //XDestroyWindow(display, [win frameWindow]);
-    //XDestroyWindow(display, [win frameWindow]);
-    //[windowsDict removeObjectForKey:[NSString stringWithFormat:@"%lu",destroyEvent.window]];
 }
 
 - (void) handleReparentNotifyEvent:(XEvent)theEvent
 {
-    /*XReparentEvent reparentEvent = theEvent.xreparent;
-    URWindow *win = [windowsDict objectForKey:[NSString stringWithFormat:@"%lu",reparentEvent.window]];
-    XReparentWindow(display,[win xWindow], [win rootWindow],0,0);*/
 }
 
 /*- (void) handleMapNotifyEvent:(XEvent)theEvent
-/
-    NSLog(@"The MapNotify event to handle: %d", theEvent.type);
-    XMapEvent mapEvent = theEvent.xmap;
-    URWindow *win = [[URWindow alloc] initWithXWindow:mapEvent.window display:display];
-    NSString *key = [NSString stringWithFormat:@"%lu",mapEvent.window];
-    //[windowsDict setObject:win forKey:key];
+{
 }*/
 
 - (void) handleMapRequestEvent:(XEvent)theEvent
@@ -84,6 +64,7 @@
     URWindow *win = [[URWindow alloc] initWithXWindow:mapRequestEvent.window display:display];
     URFrame *frameWindow = [[URFrame alloc] initWithDisplay:display];
     [frameWindow createFrameForWindow:win];
+    [frameWindow reparentChildWindow:win];
     [windowsDict setObject:frameWindow forKey:[NSString stringWithFormat:@"%lu",[win xWindow]]];
     [win mapWindow];
     NSLog(@"Il dizionario %@", windowsDict);
@@ -108,8 +89,8 @@
         XConfigureWindow(display, [frameWindow xWindow], configureRequestEvent.value_mask, &xWinChanges);
     }
     //possible errors
-    URWindow *win = [[URWindow alloc] initWithXWindow:configureRequestEvent.window display:display];
-    XConfigureWindow(display,[win xWindow],configureRequestEvent.value_mask,&xWinChanges);
+   /* URWindow *win = [[URWindow alloc] initWithXWindow:configureRequestEvent.window display:display];*/
+    XConfigureWindow(display,configureRequestEvent.window,configureRequestEvent.value_mask,&xWinChanges);
 }
 
 -(void)handleUnmapNotifyEvent:(XEvent)theEvent
@@ -174,6 +155,13 @@
         NSPoint destPosition = NSMakePoint(startFramePosition.x+delta.x,startFramePosition.y+delta.y);
         XMoveWindow(display,[frameWindow xWindow],destPosition.x, destPosition.y);
     }    
+    else if(motionEvent.state & Button3Mask)
+    {
+        NSPoint deltaSize = NSMakePoint(MAX(delta.x,-frameSize.width), MAX(delta.y,-frameSize.height));
+        NSSize finalSize = NSMakeSize(deltaSize.x+frameSize.width,deltaSize.y+frameSize.height);
+        XResizeWindow(display,[frameWindow xWindow],finalSize.width, finalSize.height);
+        XResizeWindow(display,motionEvent.window,finalSize.width, finalSize.height);
+    }
     
 }
 
